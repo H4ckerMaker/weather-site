50be960d7bf8e641ca9476fe50a09e58f62feafb
lornezo fallani <eldottorugo@gmail.com>
2022-05-28 20:06:10 +0200

    Testing MongoDB connection to DB and inserting a user

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const { MongoClient } = require('mongodb')
 const port = 5000
+const DBUrl = "mongodb+srv://eldottorugo:jnodlLDUevTit4bc@cluster0.z7bax.mongodb.net/?retryWrites=true&w=majority"
+const mongoClient = new MongoClient(DBUrl)
+const DBName = "usersDB"
 
 
 app.use(express.static('./public/dist'))
@@ -21,6 +26,27 @@ app.get('/album',(req,res)=>{
     res.render('album.ejs')
 })
 
+app.post('/login',bodyparser.urlencoded(), async function (req,res) {
+    const userData = req.body
+    insertDB("users",userData).catch(() => { 
+        console.log('Error on insert')
+    })
+})
+
 app.listen(port,()=>{
     console.log(`The server started on port ${port}`)
 })
+
+insertDB = async (collectionName, elementToInsert) => {
+    try{
+        await mongoClient.connect()
+        const database = mongoClient.db(DBName)
+        const users = database.collection(collectionName)
+        let result = await users.insertOne(
+            { 'email': elementToInsert.email, 'password': elementToInsert.password}
+        )
+        return Promise.resolve(result)
+    }catch (error) {
+        return Promise.reject()
+    }
+}
