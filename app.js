7e792df3a07b04c2b1f0c53a269b1ded4774f40c
lornezo fallani <eldottorugo@gmail.com>
2022-05-30 22:08:20 +0200

    Adding dynamic view for album page and using db for authentication

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const { MongoClient } = require('mongodb')
+const axios = require('axios').default;
 const app = express()
 const port = 5000
 const DBUrl = "mongodb+srv://eldottorugo:jnodlLDUevTit4bc@cluster0.z7bax.mongodb.net/?retryWrites=true&w=majority"
 const mongoClient = new MongoClient(DBUrl)
 const DBName = "usersDB"
+const weatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather"
+const weatherAPIKey = "793615f7609bcfaeb370a166d3a561da"
 
 
 
@@ -23,15 +26,26 @@ app.get('/login',(req,res)=>{
     res.render('login.ejs',{ error: ''})
 })
 
-app.get('/album',(req,res)=>{
-    res.render('album.ejs')
-})
 
+// Trovare un modo per passare un array di info delle citta in una sola botta e passarlo alla view altrimenti non funziona.
 app.post('/login',bodyparser.urlencoded(), async function (req,res) {
     const userData = req.body
-    getData('users', userData.email, userData.password).then(data => {
-        if(data.length)
-            res.redirect('/album')
+    const citiesInfo = []
+    checkAuth(userData.email, userData.password).then(data => {
+        if(data.length){
+            getCities(userData.email).then(results => {
+                for (var i=0;i<results.length;i++){
+                    axios.get(weatherAPIUrl + '?q=' + results[i].city + "&appid=" + weatherAPIKey).then(function (response) {
+                        citiesInfo.push(response.data.weather[0])
+                    })
+                }
+                return ()=>{
+                    res.render('album.ejs',{email: userData.email, cities: results, info: citiesInfo})
+                }
+            }).catch(error => {
+                console.log(error)
+            })
+        }
         else
             res.render('login.ejs',{ error: '!auth'})
     })
@@ -55,16 +69,30 @@ insertDB = async (collectionName, elementToInsert) => {
     }
 }
 
-getData = async (collectionName, email, password) => {
+checkAuth = async (email, password) => {
     try{
         await mongoClient.connect()
         const database = mongoClient.db(DBName)
-        const users = database.collection(collectionName)
+        const users = database.collection('users')
         let result = await users.find(
             { email: email, password: password}
         ).toArray()
         return Promise.resolve(result)
     }catch (error) {
         return Promise.reject([])
+    }
+}
+
+getCities = async (email) => {
+    try{
+        await mongoClient.connect()
+        const database = mongoClient.db(DBName)
+        const cities = database.collection('links')
+        let results = await cities.find(
+            { email: email}
+        ).toArray()
+        return Promise.resolve(results)
+    }catch (error) {
+        Promise.reject([])
     }
 }
